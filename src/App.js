11ccfacc68163d8bbe77062c3dfcfeda1fa10145
notpritam <119423845+notpritam@users.noreply.github.com>
2023-02-22 10:40:10 +0530

    Challenge Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.scss";
+import { BiErrorCircle } from "react-icons/bi";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [firstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setError(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="text">
+        <h1>Learn to code by watching others</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="form">
+        <div className="header">
+          <p>
+            <span>Try it free 7 days</span> then $20/mo. thereafter
+          </p>
+        </div>
+
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <input
+              type="text"
+              placeholder="First Name"
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+
+            {firstName.length <= 0 && error ? (
+              <>
+                <BiErrorCircle className="icons" />
+                <span className="error">First Name cannot be empty</span>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Last Name"
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+            />
+
+            {LastName.length <= 0 && error ? (
+              <>
+                <BiErrorCircle className="icons" />
+                <span className="error">Last Name cannot be empty</span>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
+          <div>
+            <input
+              type="email"
+              placeholder="Email Address"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            {email.length <= 0 && error ? (
+              <>
+                <BiErrorCircle className="icons" />
+                <span className="error">Looks like this is not an email</span>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            {password.length <= 0 && error ? (
+              <>
+                <BiErrorCircle className="icons" />
+                <span className="error">Password cannot be empty</span>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
+
+          <button>CLAIM YOUR FREE TRIAL</button>
+
+          <label className="terms">
+            By clicking the button, you are agreeing to our
+            <span className="link"> Terms and Services</span>
+          </label>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
